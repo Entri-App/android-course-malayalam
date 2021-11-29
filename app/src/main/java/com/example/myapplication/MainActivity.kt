549c47a92268bbc1f11903e155e77a836aa5a373
Sreehari K <sreeharikariot@gmail.com>
2021-11-29 23:31:58 +0530

    project added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +14,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setupUI()
+    }
+
+    private fun setupUI() {
+
+        binding.btnLogin.setOnClickListener {
+
+            val username = binding.edtUsername.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            if (username.isBlank()){
+                binding.edtUsername.setError("Invalid Username")
+                return@setOnClickListener
+            }
+            if (password.isBlank() || password.length < 3){
+                binding.edtPassword.setError("Invalid Password")
+                return@setOnClickListener
+            }
+
+            Toast.makeText(
+                this,
+                "Username is $username && password is $password",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
     }
 }
