0b890e9c31ffc5b365dfdfc2519987f11a693ebb
Sreehari K <sreeharikariot@gmail.com>
2021-12-05 23:43:18 +0530

    project added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -18,6 +19,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupUI() {
+        binding.button.setOnClickListener(this)
+        /*binding.button.setOnClickListener{
 
+        }*/
+
+        /*binding.button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+            }
+        })*/
+    }
+
+//    fun onButtonPressed(view: android.view.View) {
+//        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+//    }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
     }
 }
