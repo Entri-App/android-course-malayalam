c828b2b4c51b99fed3bf0f0d0217cf4c8e6f85ea
Sreehari K <sreeharikariot@gmail.com>
2021-12-16 02:01:30 +0530

    project added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.MediaController
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -11,6 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val myWebView = WebView(this)
+        setContentView(myWebView)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.loadUrl("https://entri.me/")
+        return
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -18,17 +25,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
-        /* binding.videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.demo_video))
-         binding.videoView.start()*/
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://entri.me/")
+        /*val unencodedHtml =
+            "<html><body>'%23' is the percent code for ‘#‘ </body></html>";
+        val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
+        binding.webView.loadData(encodedHtml, "text/html", "base64")*/
 
-        val controller = MediaController(this)
-        controller.setMediaPlayer(binding.videoView)
-        binding.videoView.setMediaController(controller)
-
-        val videoUrl =
-            "https://vod-progressive.akamaized.net/exp=1639435487~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2714%2F22%2F563574580%2F2664906044.mp4~hmac=db93cb5fe0b790b0ea021dff5aaa6e1e113494dcb0fd8c6e924b76a8582ac37b/vimeo-prod-skyfire-std-us/01/2714/22/563574580/2664906044.mp4?filename=pexels-%E5%AE%87%E8%88%AA-%E9%92%B1-8354985.mp4"
-        binding.videoView.setVideoPath(videoUrl)
-        binding.videoView.start()
     }
 
 
