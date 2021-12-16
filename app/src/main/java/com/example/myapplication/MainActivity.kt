203b1d070d18885fe53861eefc55a157d6f86e6e
Sreehari K <sreeharikariot@gmail.com>
2021-12-17 01:50:02 +0530

    project added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -11,13 +10,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val myWebView = WebView(this)
-        setContentView(myWebView)
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.loadUrl("https://entri.me/")
-        return
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -25,13 +17,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
-        binding.webView.settings.javaScriptEnabled = true
-        binding.webView.loadUrl("https://entri.me/")
-        /*val unencodedHtml =
-            "<html><body>'%23' is the percent code for ‘#‘ </body></html>";
-        val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
-        binding.webView.loadData(encodedHtml, "text/html", "base64")*/
-
+        binding.calendarView.maxDate = 1641018600000L
+        binding.calendarView.minDate = 1609482600000L
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val date = "$year/${month + 1}/$dayOfMonth"
+            binding.textView.text = date
+        }
     }
 
 
