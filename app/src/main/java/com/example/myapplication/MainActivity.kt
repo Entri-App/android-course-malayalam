e267a47480e8437fc3290625c0fe55bf28dfa642
Sreehari K <sreehari.kt@sunnyclock.com>
2021-12-27 15:05:46 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayoutMediator
@@ -25,7 +27,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
+        //get fragmentContainerView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        //find navigation controller
+        NavigationUI.setupWithNavController(binding.bottomNav,navController)
 
+        //connect nav graph with bottom nav viee
     }
 
 
