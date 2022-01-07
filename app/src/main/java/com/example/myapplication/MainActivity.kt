279b815ba8b6f15ea6d5719eb37ba2697a69bce8
Sreehari K <sreehari.kt@sunnyclock.com>
2022-01-07 23:26:17 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -17,7 +21,58 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
+        val array = getData()
+        //TODO : Create adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, array)
+        //TODO : Assign adapter to listview
+        binding.listView.adapter = adapter
+        //TODO : Implement onClickListner
+        binding.listView.onItemClickListener =
+            AdapterView.OnItemClickListener { p0, p1, p2, p3 ->
+                val clickedItem = array[p2]
+                Toast.makeText(this, clickedItem, Toast.LENGTH_SHORT).show()
+            }
+    }
 
+    fun getData(): Array<String> {
+        return arrayOf(
+            "Andhra Pradesh",
+            "Arunachal Pradesh",
+            "Assam",
+            "Bihar",
+            "Chhattisgarh",
+            "Goa",
+            "Gujarat",
+            "Haryana",
+            "Himachal Pradesh",
+            "Jammu and Kashmir",
+            "Jharkhand",
+            "Karnataka",
+            "Kerala",
+            "Madhya Pradesh",
+            "Maharashtra",
+            "Manipur",
+            "Meghalaya",
+            "Mizoram",
+            "Nagaland",
+            "Odisha",
+            "Punjab",
+            "Rajasthan",
+            "Sikkim",
+            "Tamil Nadu",
+            "Telangana",
+            "Tripura",
+            "Uttarakhand",
+            "Uttar Pradesh",
+            "West Bengal",
+            "Andaman and Nicobar Islands",
+            "Chandigarh",
+            "Dadra and Nagar Haveli",
+            "Daman and Diu",
+            "Delhi",
+            "Lakshadweep",
+            "Puducherry"
+        )
     }
 
 
