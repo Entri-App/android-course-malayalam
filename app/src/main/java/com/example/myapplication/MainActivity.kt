b3360fd0027894780437312f1b7e9dafc0a1205e
Sreehari K <sreehari.kt@sunnyclock.com>
2022-01-17 00:40:56 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -32,28 +32,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun checkPermission(view: View) {
-        ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.CAMERA
-        ) == PackageManager.PERMISSION_GRANTED -> {
-            // You can use the API that requires the permission.
-        }
-        shouldShowRequestPermissionRationale(...) -> {
-            // In an educational UI, explain to the user why your app requires this
-            // permission for a specific feature to behave as expected. In this UI,
-            // include a "cancel" or "no thanks" button that allows the user to
-            // continue using your app without granting the permission.
-
-        }
-        else -> {
-            // You can directly ask for the permission.
-            // The registered ActivityResultCallback gets the result of this request.
-            requestPermissionLauncher.launch(
-                Manifest.permission.REQUESTED_PERMISSION)
-        }
-    }
-
     fun checkPermissionInNewWay(view: android.view.View) {}
     fun checkPermissionInOldWay(view: android.view.View) {}
 }
