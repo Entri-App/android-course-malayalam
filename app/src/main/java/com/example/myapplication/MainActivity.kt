55ed3c700d69c9107a1104a9bd1771ac627fac49
Sreehari K <sreehari.kt@sunnyclock.com>
2022-01-17 23:47:00 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.app.Activity
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -18,6 +23,8 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val CAMERA_REQ_CODE = 123
+    private lateinit var request: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +35,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
-
+        request =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    Toast.makeText(this, "Read contact permission granted", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(this, "Read contact permission denied!!!!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
 
     }
 
-    fun checkPermissionInNewWay(view: android.view.View) {}
-    fun checkPermissionInOldWay(view: android.view.View) {}
+    //Requests permissions with ActivityResultContracts
+    fun checkPermissionInNewWay(view: android.view.View) {
+        request.launch(Manifest.permission.READ_CONTACTS)
+    }
+
+    //Function to check runtime permission in old way.
+    fun checkPermissionInOldWay(view: android.view.View) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_REQ_CODE)
+        } else {
+            //Continue with feature
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_REQ_CODE) {
+            if (grantResults[0] == PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Camera permission denied!!!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
 }
