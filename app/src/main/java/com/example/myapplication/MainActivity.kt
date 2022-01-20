8a6d7c234276f9b8aca80b714d98728c301cfeca
Sreehari K <sreehari.kt@sunnyclock.com>
2022-01-21 01:34:55 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,6 +26,8 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var request: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,5 +37,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
+
+        request =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    val phone = binding.txtPhone.text.toString()
+                    val uri = "tel:+91$phone"
+                    val intent = Intent(ACTION_CALL, Uri.parse(uri))
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Permission is missing", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        binding.btnCall.setOnClickListener {
+            val phone = binding.txtPhone.text.toString()
+            if (phone.isBlank() || phone.length < 8) {
+                binding.txtPhone.error = "Invalid Number"
+                return@setOnClickListener
+            }
+            request.launch(Manifest.permission.CALL_PHONE)
+
+        }
     }
 }
