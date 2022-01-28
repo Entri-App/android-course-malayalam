e0431afe17cb2a7c9395368376dc5dad6b1ddc5b
Sreehari K <sreehari.kt@sunnyclock.com>
2022-01-29 00:09:04 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -17,9 +20,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
-        //same as day 92
-        binding.btnShowAlert.setOnClickListener {
+        binding.btnShowToast.setOnClickListener {
+            val toast = Toast.makeText(this, "Simple Toast", Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.BOTTOM or Gravity.RIGHT, 0, 0)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                toast.addCallback(object : Toast.Callback() {
+                    override fun onToastShown() {
+                        super.onToastShown()
+                    }
 
+                    override fun onToastHidden() {
+                        super.onToastHidden()
+                    }
+                })
+            }
+            toast.show()
         }
     }
 }
