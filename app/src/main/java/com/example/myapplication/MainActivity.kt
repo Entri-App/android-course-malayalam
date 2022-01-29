05e030b84c420f5c4a96c246e3f22f073f8bc084
Sreehari K <sreehari.kt@sunnyclock.com>
2022-01-30 01:53:06 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,8 +4,10 @@ import android.R.layout
 import android.os.Bundle
 import android.view.Gravity
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.example.myapplication.databinding.ActivityMainBinding
 
 
@@ -28,5 +30,62 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.main_menu, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_profile -> {
+                Toast.makeText(this, "Profile is clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_search -> {
+                val searchView = item.actionView as SearchView
+                setupSearchView(searchView)
+            }
+            R.id.menu_edit -> {
+                Toast.makeText(this, "Edit is clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_delete -> {
+                Toast.makeText(this, "Delete is clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_settings -> {
+                Toast.makeText(this, "Settings is clicked", Toast.LENGTH_SHORT).show()
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+    private fun setupSearchView(searchView: SearchView) {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+    }
+
+    /*fun onClickMenu(item: MenuItem) {
+        when (item.itemId) {
+            R.id.menu_profile -> {
+                Toast.makeText(this, "Profile is clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_search -> {
+                Toast.makeText(this, "Search is clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_edit -> {
+                Toast.makeText(this, "Edit is clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_delete -> {
+                Toast.makeText(this, "Delete is clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_settings -> {
+                Toast.makeText(this, "Settings is clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }*/
 
 }
