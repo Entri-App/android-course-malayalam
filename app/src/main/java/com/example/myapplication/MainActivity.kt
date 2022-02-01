a81f30d08ad5f0b3b38fe4847f7870639f2f6247
Sreehari K <sreehari.kt@sunnyclock.com>
2022-02-02 00:08:52 +0530

    popup menu implementation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.*
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -9,7 +10,6 @@ import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var actionMode: ActionMode? = null
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,90 +20,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupUI() {
-        registerForContextMenu(binding.tvFloating)
-        binding.tvActionMode.setOnLongClickListener {
-            if (actionMode != null) {
-                return@setOnLongClickListener false
-            }
-            actionMode = startActionMode(actionModeCallback)
-            return@setOnLongClickListener true
+        binding.btnShowMenu.setOnClickListener {
+            val menu = PopupMenu(this, it)
+            menu.inflate(R.menu.main_menu)
+            menu.setOnMenuItemClickListener(menuCallback)
+            menu.show()
         }
     }
 
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        val inflator = menuInflater
-        inflator.inflate(R.menu.main_menu, menu)
-    }
-
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_profile -> {
-                Toast.makeText(this@MainActivity, "menu_profile clicked", Toast.LENGTH_SHORT).show()
+    val menuCallback = PopupMenu.OnMenuItemClickListener {
+        when (it.itemId) {
+            R.id.menu_settings -> {
+                Toast.makeText(this, "menu_settings clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.menu_delete -> {
-                Toast.makeText(this@MainActivity, "menu_delete clicked", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "menu_delete clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.menu_edit -> {
-                Toast.makeText(this@MainActivity, "menu_edit clicked", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "menu_edit clicked", Toast.LENGTH_SHORT).show()
             }
-            R.id.menu_settings -> {
-                Toast.makeText(this@MainActivity, "menu_settings clicked", Toast.LENGTH_SHORT)
-                    .show()
+            R.id.menu_search -> {
+                Toast.makeText(this, "menu_search clicked", Toast.LENGTH_SHORT).show()
             }
-            else -> return super.onContextItemSelected(item)
+            R.id.menu_profile -> {
+                Toast.makeText(this, "menu_profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_about -> {
+                Toast.makeText(this, "menu_about clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_contact -> {
+                Toast.makeText(this, "menu_contact clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_legal -> {
+                Toast.makeText(this, "menu_legal clicked", Toast.LENGTH_SHORT).show()
+            }
         }
-        return true
-
+        return@OnMenuItemClickListener true
     }
-
-
-    private val actionModeCallback = object : ActionMode.Callback {
-        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
-            val inflater = mode.menuInflater
-            inflater.inflate(R.menu.main_menu, menu)
-            mode.title = "Choose an action"
-            return true
-        }
-
-        override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
-            return false
-        }
-
-        override fun onActionItemClicked(p0: ActionMode?, item: MenuItem): Boolean {
-            return when (item.itemId) {
-                R.id.menu_profile -> {
-                    Toast.makeText(this@MainActivity, "menu_profile clicked", Toast.LENGTH_SHORT)
-                        .show()
-                    true
-                }
-                R.id.menu_delete -> {
-                    Toast.makeText(this@MainActivity, "menu_delete clicked", Toast.LENGTH_SHORT)
-                        .show()
-                    true
-                }
-                R.id.menu_edit -> {
-                    Toast.makeText(this@MainActivity, "menu_edit clicked", Toast.LENGTH_SHORT)
-                        .show()
-                    true
-                }
-                R.id.menu_settings -> {
-                    Toast.makeText(this@MainActivity, "menu_settings clicked", Toast.LENGTH_SHORT)
-                        .show()
-                    true
-                }
-                else -> false
-            }
-        }
-
-        override fun onDestroyActionMode(p0: ActionMode?) {
-            actionMode = null
-        }
-
-    }
-
 }
