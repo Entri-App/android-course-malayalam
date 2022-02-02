67c2ff669b91312413447d1bb311b2b2a66cb2fd
Sreehari K <sreehari.kt@sunnyclock.com>
2022-02-03 01:41:17 +0530

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,37 +12,9 @@ import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.d(TAG, "Inside onResume")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "Inside onRestart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "Inside onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "Inside onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "Inside onDestroy")
-    }
-
 }
