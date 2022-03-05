94da75bc3bac69739434e17132a09bbe7b6854c9
Sreehari K <sreeharikariot@gmail.com>
2022-03-05 21:11:56 +0530

    starter code

--- app/src/main/java/com/example/myapplication/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activities/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.myapplication.ui.activities
 
-import android.app.ProgressDialog
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.*
-import com.android.volley.toolbox.JsonObjectRequest
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.utils.PrefUtils
-import com.example.myapplication.volley.MySingleton
-import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -39,48 +33,6 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Invalid Password", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-
-            val url = "https://reqres.in/api/login"
-
-            val reqObj = JSONObject()
-            reqObj.put("email", username)
-            reqObj.put("password", password)
-
-            Log.d(TAG, "JSON : $reqObj")
-
-            val progressDialog = ProgressDialog(this)
-            progressDialog.setMessage("Please wait..")
-            progressDialog.setCancelable(false)
-            progressDialog.show()
-
-            val request = JsonObjectRequest(Request.Method.POST, url, reqObj, { response ->
-                progressDialog.dismiss()
-                val token = response.getString("token")
-                checkSharedPref()
-                DashbaordActivity.start(this, username, password)
-                finish()
-            }, { error ->
-                progressDialog.dismiss()
-                if (error.networkResponse != null) {
-                    if (error.networkResponse.statusCode == 400) {
-                        Toast.makeText(this, "Invalid username and password", Toast.LENGTH_SHORT)
-                            .show()
-                        return@JsonObjectRequest
-                    }
-                }
-                val message = when (error) {
-                    is NetworkError -> "Network error occurred"
-                    is ServerError -> "Server error occurred"
-                    is AuthFailureError -> "Authentication error"
-                    is ParseError -> "Parse error"
-                    is NoConnectionError -> "No connection."
-                    is TimeoutError -> "Request timed out."
-                    else -> "An error occurred"
-                }
-                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-            })
-            MySingleton.getInstance(this).addToRequestQueue(request)
-
         }
 
 
