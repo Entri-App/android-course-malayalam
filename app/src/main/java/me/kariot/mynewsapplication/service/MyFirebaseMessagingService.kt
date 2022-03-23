9661afd1c092734fe4b0ead3ee0099f6f289f154
Sreehari K <sreeharikariot@gmail.com>
2022-03-24 00:19:31 +0530

    progress

--- app/src/main/java/me/kariot/mynewsapplication/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/me/kariot/mynewsapplication/service/MyFirebaseMessagingService.kt
@@ -1,15 +1,35 @@
 package me.kariot.mynewsapplication.service
 
+import android.app.Notification
+import android.os.Build
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import me.kariot.mynewsapplication.R
 
+const val TAG = "NewsApp"
 class MyFirebaseMessagingService : FirebaseMessagingService() {
-
-    override fun onNewToken(token: String) {
-        super.onNewToken(token)
-    }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        val title = message.notification?.title ?: ""
+        val body = message.notification?.body ?: ""
+
+        val CHANNEL_ID = "NEWS_NOTIFICATION"
+        val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.ic_article)
+                .setAutoCancel(false)
+        } else {
+            Notification.Builder(this)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.ic_article)
+                .setAutoCancel(false)
+        }
+        NotificationManagerCompat.from(this).notify(1, notification.build())
+
     }
 }
