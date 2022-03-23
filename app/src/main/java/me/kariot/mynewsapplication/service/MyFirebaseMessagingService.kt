842fef15a52d6b4bcfe9e5084f6f79a6c62416e0
Sreehari K <sreeharikariot@gmail.com>
2022-03-24 00:34:32 +0530

    foreground notifications added

--- app/src/main/java/me/kariot/mynewsapplication/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/me/kariot/mynewsapplication/service/MyFirebaseMessagingService.kt
@@ -1,6 +1,8 @@
 package me.kariot.mynewsapplication.service
 
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.os.Build
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -16,6 +18,14 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val body = message.notification?.body ?: ""
 
         val CHANNEL_ID = "NEWS_NOTIFICATION"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "General Pushes",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
+        }
         val notification = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             Notification.Builder(this, CHANNEL_ID)
                 .setContentTitle(title)
