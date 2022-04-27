2382e787113b94502fad16b98e11200650467989
Sreehari K <sreeharikariot@gmail.com>
2022-04-27 22:39:33 +0530

    added contact us alert

--- app/src/main/java/me/kariot/mynewsapplication/ui/MainActivity.kt
+++ app/src/main/java/me/kariot/mynewsapplication/ui/MainActivity.kt
@@ -1,12 +1,36 @@
 package me.kariot.mynewsapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import me.kariot.mynewsapplication.R
+import me.kariot.mynewsapplication.databinding.LayoutContactUsAlertBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.contact_us) {
+            showContactUsAlert()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showContactUsAlert() {
+        AlertDialog.Builder(this).apply {
+            setView(LayoutContactUsAlertBinding.inflate(layoutInflater).root)
+        }.create().show()
     }
 }
